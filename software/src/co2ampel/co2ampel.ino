ae85d7ad938adfc825814c7f9c0e3d502e8a1138
Gernot H. Reichel <gernot.reichel@me.com>
2020-12-27 17:26:07 +0100

    removed Wifi stuff (instable...) and added Potentiometer to modify the trigger level

--- software/src/co2ampel/co2ampel.ino
+++ software/src/co2ampel/co2ampel.ino
@@ -1,58 +1,4 @@
 /*
- * Copyright (C) 2017 Robert Bosch. All Rights Reserved. 
- *
- * Disclaimer
- *
- * Common:
- * Bosch Sensortec products are developed for the consumer goods industry. They may only be used
- * within the parameters of the respective valid product data sheet.  Bosch Sensortec products are
- * provided with the express understanding that there is no warranty of fitness for a particular purpose.
- * They are not fit for use in life-sustaining, safety or security sensitive systems or any system or device
- * that may lead to bodily harm or property damage if the system or device malfunctions. In addition,
- * Bosch Sensortec products are not fit for use in products which interact with motor vehicle systems.
- * The resale and/or use of products are at the purchasers own risk and his own responsibility. The
- * examination of fitness for the intended use is the sole responsibility of the Purchaser.
- *
- * The purchaser shall indemnify Bosch Sensortec from all third party claims, including any claims for
- * incidental, or consequential damages, arising from any product use not covered by the parameters of
- * the respective valid product data sheet or not approved by Bosch Sensortec and reimburse Bosch
- * Sensortec for all costs in connection with such claims.
- *
- * The purchaser must monitor the market for the purchased products, particularly with regard to
- * product safety and inform Bosch Sensortec without delay of all security relevant incidents.
- *
- * Engineering Samples are marked with an asterisk (*) or (e). Samples may vary from the valid
- * technical specifications of the product series. They are therefore not intended or fit for resale to third
- * parties or for use in end products. Their sole purpose is internal client testing. The testing of an
- * engineering sample may in no way replace the testing of a product series. Bosch Sensortec
- * assumes no liability for the use of engineering samples. By accepting the engineering samples, the
- * Purchaser agrees to indemnify Bosch Sensortec from all claims arising from the use of engineering
- * samples.
- *
- * Special:
- * This software module (hereinafter called "Software") and any information on application-sheets
- * (hereinafter called "Information") is provided free of charge for the sole purpose to support your
- * application work. The Software and Information is subject to the following terms and conditions:
- *
- * The Software is specifically designed for the exclusive use for Bosch Sensortec products by
- * personnel who have special experience and training. Do not use this Software if you do not have the
- * proper experience or training.
- *
- * This Software package is provided `` as is `` and without any expressed or implied warranties,
- * including without limitation, the implied warranties of merchantability and fitness for a particular
- * purpose.
- *
- * Bosch Sensortec and their representatives and agents deny any liability for the functional impairment
- * of this Software in terms of fitness, performance and safety. Bosch Sensortec and their
- * representatives and agents shall not be liable for any direct or indirect damages or injury, except as
- * otherwise stipulated in mandatory applicable law.
- *
- * The Information provided is believed to be accurate and reliable. Bosch Sensortec assumes no
- * responsibility for the consequences of use of such Information nor for any infringement of patents or
- * other rights of third parties which may result from its use. No license is granted by implication or
- * otherwise under any patent or patent rights of Bosch. Specifications mentioned in the Information are
- * subject to change without notice.
- *
  * It is not allowed to deliver the source code of the Software to any third party without permission of
  * Bosch Sensortec.
  *
@@ -76,19 +22,14 @@
 /**********************************************************************************************************************/
 
 #include "bsec_integration.h"
-#include "IAQFifo.h"
-#include "SimpleFIFO.h"
 #include <Wire.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-
-// #include <Arduino_JSON.h>
 
 /**********************************************************************************************************************/
 /* Hardware Pins */
 /**********************************************************************************************************************/
-#define LED_GOOD_PIN INT8_C(12) // GPIO12 -> D6
-#define LED_BAD_PIN INT8_C(13)  // GPIO13 -> D7
+const int LED_GOOD_PIN = 12; // GPIO12 -> D6
+const int LED_BAD_PIN = 13;  // GPIO13 -> D7
+const int POTI_PIN = A0;
 
 /**********************************************************************************************************************/
 /* local variables */
@@ -262,6 +203,7 @@ void setup()
     /* Init the PED Pins */
     pinMode(LED_GOOD_PIN, OUTPUT);
     pinMode(LED_BAD_PIN, OUTPUT);
+    pinMode(POTI_PIN, INPUT);
 
     /* Init I2C and serial communication */
     Wire.begin(0, 2);
@@ -282,9 +224,6 @@ void setup()
         Serial.println("Error while initializing BSEC library");
         return;
     }
-
-    //scanNetworks();
-    //connectWifi();
 
     /* Call to endless loop function which reads and processes data based on sensor settings */
     /* State is saved every 10.000 samples, which means every 10.000 * 3 secs = 500 minutes  */
@@ -364,96 +303,17 @@ void handle_led()
         digitalWrite(LED_GOOD_PIN, HIGH);
         digitalWrite(LED_BAD_PIN, HIGH);
     }
-
+    /*
     Serial.print(strState);
     Serial.print(" (state=");
     Serial.print(state);
     Serial.print(")");
     Serial.println();
-}
-
-void scanNetworks()
-{
-    // scan for nearby networks:
-    Serial.println("** Scan Networks **");
-    byte numSsid = WiFi.scanNetworks();
-
-    // print the list of networks seen:
-    Serial.print("SSID List:");
-    Serial.println(numSsid);
-    // print the network number and name for each network found:
-    for (int thisNet = 0; thisNet < numSsid; thisNet++)
-    {
-        Serial.print(thisNet);
-        Serial.print(") Network: ");
-        Serial.println(WiFi.SSID(thisNet));
-    }
-}
-
-void connectWifi()
-{
-
-    WiFi.begin(WLAN_SSID, WLAN_PASSWD); // Connect to the network
-    Serial.print("Connecting to ");
-    Serial.print(WLAN_SSID);
-    Serial.println(" ...");
-
-    int i = 0;
-    while (WiFi.status() != WL_CONNECTED)
-    { // Wait for the Wi-Fi to connect
-        delay(1000);
-        Serial.print(++i);
-        Serial.print("..");
-    }
-
-    Serial.println('\n');
-    Serial.println("Connection established!");
-    Serial.print("IP address:\t");
-    Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+*/
 }
 
 void sendData(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temperature, float humidity, float pressure)
 {
-    Serial.print(iaq);
-    /*
-    // try to reconnect three times if not connected
-    for (int n = 0; n < 3 && WiFi.status() != WL_CONNECTED; n++)
-    {
-        connectWifi();
-    }
-
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        HTTPClient http;
-        WiFiClient client;
-        http.setReuse(true);
-        http.begin(client, "http://raspbox:1880/api/test");
-        http.addHeader("Content-Type", "application/json");
-
-        String body;
-        body += F("{\"iaq_accuracy\":\"");
-        body += iaq_accuracy;
-        body += F("\",\"iaq\":\"");
-        body += String(iaq, 2);
-        body += F("\",\"temperature\":\"");
-        body += String(temperature, 2);
-        body += F("\",\"humidity\":\"");
-        body += String(humidity, 2);
-        body += F("\",\"pressure\":\"");
-        body += String(pressure, 2);
-        body += F("\"}");
-
-        int httpResponseCode = http.POST(body);
-        http.end();
-
-        Serial.print("; status code: ");
-        Serial.print(httpResponseCode);
-    }
-    else
-    {
-        Serial.println("Error in WiFi connection");
-    }
-    */
 }
 
 /* ========================================================================== */
@@ -479,12 +339,36 @@ void sendData(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temperat
 void output_ready(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temperature, float humidity,
                   float pressure, float raw_temperature, float raw_humidity, float gas, bsec_library_return_t bsec_status)
 {
+    /*
     Serial.print("iaq_accuracy=");
     Serial.print(iaq_accuracy);
     Serial.print(", iaq=");
     Serial.print(iaq);
     Serial.print(", bsec_status=");
     Serial.print(bsec_status);
+    */
+    /**
+    * Analog read value: [0; 1024]
+    * iaq value:         [0; 500]
+    * delta = max-min
+    * offset = 150 - delta/2
+    * 
+    * trigger value sould be trimmed between [100; 200] => f(x) = analog * delta / 1024 + offset
+    * 
+    * Example 1: offset = 100, delta = 100 => f(x)=analog * x/10,24 + 100
+    * Example 2: offset = 130, delta = 40  => f(x)=analog * x/25,6 + 130
+    * Example 3: offset = 50,  delta = 200 => f(x)=analog * x/5,12 + 50
+    */
+    float delta = 100.0;
+    float offset = 150.0 - delta / 2;
+
+    float triggerValue = analogRead(POTI_PIN) * delta / 1024.0 + offset;
+
+    Serial.print(iaq_accuracy);
+    Serial.print(" ");
+    Serial.print(iaq);
+    Serial.print(" ");
+    Serial.println(triggerValue);
 
     if (bsec_status == BSEC_OK)
     {
@@ -495,37 +379,13 @@ void output_ready(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temp
         }
         else
         {
-            if (iaq < 0)
-            {
-                state = STATE_RAMPUP;
-            }
-            else if (iaq >= 0 && iaq <= 50)
-            {
-                state = STATE_GOOD;
-            }
-            else if (iaq > 50 && iaq <= 100)
+            if (iaq < triggerValue)
             {
                 state = STATE_OK;
             }
-            else if (iaq > 100 && iaq <= 150)
-            {
-                state = STATE_SOLALA;
-            }
-            else if (iaq > 150 && iaq <= 200)
-            {
-                state = STATE_BAD;
-            }
-            else if (iaq > 200 && iaq <= 300)
-            {
-                state = STATE_REALLYBAD;
-            }
-            else if (iaq > 300 && iaq <= 500)
-            {
-                state = STATE_SUPERBAD;
-            }
             else
             {
-                state = STATE_UNDEFINED;
+                state = STATE_BAD;
             }
         }
     }
